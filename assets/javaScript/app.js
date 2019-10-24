cf01a0914214495fd4cfa1d70f8e0264dff62e22
Bijan Dayani <bdayani518@gmail.com>
2019-10-23 23:14:58 -0400

    added play/pause gif function

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-
-    var options = ['Pogs', '90s Taco Bell', 'Furby', 'Tamagotchi Toy', 'Aerosmith', 'Trolls 90s', 'Street Sharks'];
-    var stillIMG = '';
-
-
+    // Default topics array
+    var topics = ['Pogs', '90s Taco Bell', 'Furby', 'Tamagotchi Toy', 'Aerosmith', 'Trolls 90s', 'Street Sharks'];
+    
+    
+   
+   
     // creating buttons
     function createButtons(){
         
         $('#button-spot').empty();
         
-        // iterate through options array
-        for (let o = 0; o < options.length; o++) {
-            const selection = options[o];
+        // iterate through topics array
+        for (let t = 0; t < topics.length; t++) {
+            const selection = topics[t];
             
             // Materialize button tag variable
             var button = $('<a class="waves-effect orange waves-light btn-large"</a>');
@@ -24,24 +25,42 @@ $(document).ready(function(){
         }
     }
 
-    // click function for adding new item to options array
+    // click function for adding new item to topics array
     $('#submit-button').on('click', function(){
         event.preventDefault();
 
         // save input value as a variable
         var newGif = $('#gif_selection').val().trim();
-        // push input value data to options array
-        options.push(newGif);
+        // push input value data to topics array
+        topics.push(newGif);
+        // call createButtons again, now that newGif has been added to topics array
         createButtons();
         
     })
 
     // Run createButtons function
     createButtons();
+
+    // Create function for starting and stopping gifs
+    $(document).on('click', '.gif', function(){
+
+        var state = $(this).attr('data-type');
+
+        if (state === 'still') {
+            // if data type = 'still', change 'src' attr to 'data-animate' and change 'data-type' attr to 'animate'
+            $(this).attr('src', $(this).attr('data-animate'));
+            $(this).attr('data-type', 'animate');
+        } else {
+            // else change back to 'data-still' and 'still'
+            $(this).attr('src', $(this).attr('data-still'));
+            $(this).attr('data-type', 'still');
+        }
+
+    })
     
     
     // click event listener on buttons
-    $('.btn-large').on('click', function() {
+    $(document).on('click', '.btn-large', function() {
         
         // clear previously displayed results
         $('#gif-spot').empty();
@@ -59,14 +78,17 @@ $(document).ready(function(){
         // after making request, then...
         .then(function(response){
             // console log response
-            console.log(response);
             
             // store returned data as results variable
             var results = response.data;
             
             // iterate through results items
             for (let r = 0; r < results.length; r++) {
-                const datGif = results[r];
+                // create variables for setting img attributes
+                var datGif = results[r];
+                var stillImgUrl = datGif.images.fixed_height_still.url;
+                var imgURL = datGif.images.fixed_height.url;
+                
                 
                 // create parent div for datGif content
                 var gifDiv = $('<div>');
@@ -75,9 +97,13 @@ $(document).ready(function(){
                 var gifRating = $('<p>').text('Rating: '+datGif.rating);
                 
                 // create image tag and setting src attr to datGif image
-                var gifImage = $('<img>');
-                gifImage.attr('src', datGif.images.fixed_height.url);
-                
+                var gifImage = $('<img class="gif">');
+                gifImage.attr('src', stillImgUrl);
+                gifImage.attr('data-still', stillImgUrl);
+                gifImage.attr('data-animate', imgURL);
+                // gif loads in a still state
+                gifImage.attr('data-type', 'still');
+
                 // append gifRating and gifImage to gifDiv
                 gifDiv.append(gifRating);
                 gifDiv.append(gifImage);
@@ -88,7 +114,11 @@ $(document).ready(function(){
                 
             }
         })
-        
-        
     })
 })
+
+
+
+
+
+
